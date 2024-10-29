a17a77b9c04d312759521f947a9f1d339d863315
raizo <admin@raizo.dev>
2024-10-29 13:37:53 +0100

    added multiple plugins and theme

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
 require("config.lazy")
+require("lazy")
+require("mason").setup()
+require("mason-lspconfig").setup()
+require'lspconfig'.typst_lsp.setup{
+	settings = {
+		exportPdf = "onType" -- Choose onType, onSave or never.
+        -- serverPath = "" -- Normally, there is no need to uncomment it.
+	}
+}
+
+vim.cmd("colorscheme rose-pine")
+
+vim.wo.relativenumber = true
+
+local map = vim.keymap.set
+
+map("", "<C-f>", ":Files<CR>")
